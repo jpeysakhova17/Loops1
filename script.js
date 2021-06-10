821a63b3c970f9a4a68e7406281c03ae8104b655
Jenny <jenny@elis-MacBook-Air.local>
2021-06-10 18:37:39 -0400

    done

---
+++ script.js
@@ -0,0 +1,36 @@
+for (let i = 0; i<=10; i++) {
+    console.log(i);
+}
+
+for (let i=10; i>=0; i--) {
+    console.log(i);
+}
+
+let i=0;
+while (i<=10) {
+    console.log(i);
+    i++;
+}
+
+let a=10;
+while (a>=0) {
+    console.log(a);
+    a--;
+}
+
+do {
+   console.log(i);
+   i++;
+}
+while (i<=10);
+
+do {
+    console.log(a);
+    a++;
+}
+while (a>=10);
+
+let myarray = [1,2,3,4,5,6,7,8,9,10]
+for (let i=0; i<myarray.length; i++) {
+    console.log(myarray[i])
+}
